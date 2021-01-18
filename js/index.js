7ea0e51d14225f49b5a5d48ed84db1f0ba84db12
Oleksandr Shchegol <oleksandr@truckpartsolutions.com>
2021-01-17 19:19:10 -0700

    project section

--- js/index.js
+++ js/index.js
@@ -29,7 +29,15 @@ window.addEventListener('load', function()
         }
     });
     
-
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      });
 
 
 }, false);
