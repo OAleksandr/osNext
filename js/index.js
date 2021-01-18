8b5f5a0640da6c858aec8611d81e7b64cfbb85e4
Oleksandr Shchegol <oleksandr@truckpartsolutions.com>
2021-01-17 19:37:00 -0700

    swiper added to project section

--- js/index.js
+++ js/index.js
@@ -32,10 +32,14 @@ window.addEventListener('load', function()
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 30,
-        freeMode: true,
+        loop: true,
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
       });
 
