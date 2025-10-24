3e8e38126bb5ee22ac62a592e976947edce1fd2c
Oleksandr Shchegol <oleksandr93@yahoo.ca>
2025-10-23 22:23:46 -0600

    Next App project and Chapter 2

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-200 text-center p-4 text-sm text-gray-700">
-      © {new Date().getFullYear()} My osNext App — All rights reserved.
-    </footer>
-  );
-}
