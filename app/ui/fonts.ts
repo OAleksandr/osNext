1075a8a739f863abb9c063c45ad9c535b6a2f768
Oleksandr Shchegol <oleksandr93@yahoo.ca>
2025-10-25 20:51:32 -0600

    Chapter 3 Fonts and Images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Audiowide, Lusitana, Inter } from 'next/font/google';
+ 
+export const inter = Inter({ 
+    subsets: ['latin'] 
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const audiowide = Audiowide({ 
+    weight: ['400'], 
+    subsets: ['latin'] 
+});
